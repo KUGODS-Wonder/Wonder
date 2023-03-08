c325920feba85af1f33abd79f41c35914c40a2a8
keo88 <keo.kim0808@gmail.com>
2023-03-08 22:59:17 +0900

    fix: add json files, extend app walk theme logics to cover special wonder walks implementation on event screens. related to: #1

--- lib/app/common/values/styles/app_walk_theme_style.dart
+++ lib/app/common/values/styles/app_walk_theme_style.dart
@@ -4,14 +4,24 @@ import 'package:wonder_flutter/app/common/values/app_colors.dart';
 abstract class AppWalkThemeStyle {
   static const defaultWalkThemeStyle = WalkThemeStyleModel(
     color: AppColors.defaultWalkColor,
-    path: 'assets/images/walk_theme_default.png',
+    iconPath: 'assets/images/walk_theme_default.png',
   );
 
   static final Map<String, WalkThemeStyleModel> _dict = {
     '일반 테마': defaultWalkThemeStyle,
     '동네 맛집 투어': const WalkThemeStyleModel(
       color: AppColors.foodTourColor,
-      path: 'assets/images/walk_theme_food_tour.png',
+      iconPath: 'assets/images/walk_theme_food_tour.png',
+    ),
+    '도시락 전달': const WalkThemeStyleModel(
+      color: AppColors.kPrimary80,
+      iconPath: 'assets/images/elderly_icon.png',
+      eventCardPath: 'assets/images/event_cards/help_elderly_event_card.png',
+    ),
+    '유기견 산책': const WalkThemeStyleModel(
+      color: AppColors.kPrimary80,
+      iconPath: 'assets/images/dog_icon.png',
+      eventCardPath: 'assets/images/event_cards/walk_dog_event_card.png',
     ),
   };
 
@@ -21,8 +31,15 @@ abstract class AppWalkThemeStyle {
 }
 
 class WalkThemeStyleModel {
-  final Color color;
-  final String path;
+  static const defaultEventCardPath = 'assets/images/default_event_card.png';
 
-  const WalkThemeStyleModel({required this.color, required this.path});
+  final Color color;
+  final String iconPath;
+  final String eventCardPath;
+
+  const WalkThemeStyleModel({
+    required this.color,
+    required this.iconPath,
+    this.eventCardPath = defaultEventCardPath,
+  });
 }
